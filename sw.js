c045978c4e0402c9026cb4e239bfaae6b3ef14d5
Brody730 <127863094+Brody730@users.noreply.github.com>
2026-07-16 23:33:19 -0600

    3

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    sin conexión (en el parque sin datos).
    ═══════════════════════════════════════════ */
 
-var CACHE_NAME = 'cheskoretos-v4';
+var CACHE_NAME = 'cheskoretos-v5';
 var URLS_TO_CACHE = [
   '/',
   '/index.html',
